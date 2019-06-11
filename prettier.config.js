078265120ec57458b6d9b277c655eb5cd0d51c16
Tamas Besenyei <tamas@besenyei.net>
2019-06-11 09:01:27 +0200

    version bump

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    printWidth: 120,
+    useTabs: false,
+    semi: true,
+    singleQuote: true,
+    trailingComma: 'none',
+    bracketSpacing: true,
+    arrowParens: 'avoid',
+    tabWidth: 4
+};
