f7cfa502a1ebc2570d7743f7099cb901ca2707ca
Anton Tsymuk <atsymuk@gmail.com>
2019-08-02 10:31:22 +0300

    Revert "Merge pull request #50 from oat-sa/feature/TAO-8725_update-interact-js-lib"
    This reverts commit 5ee58752dab9890e1ac301aa954f1495dbee534f, reversing
    changes made to 310d2ba45ff4f02de2ea0017e209e8ca9c86ca97.

--- src/component/resizable.js
+++ src/component/resizable.js
@@ -27,7 +27,7 @@
  * @author Christophe Noël <christophe@taotesting.com>
  */
 import _ from 'lodash';
-import interact from 'interactjs';
+import interact from 'interact';
 import makePlaceable from 'ui/component/placeable';
 
 var defaultConfig = {
@@ -162,8 +162,6 @@ export default function makeResizable(component, config) {
             var self = this,
                 $element = this.getElement(),
                 element = $element[0];
-
-            $element.css('touch-action', 'none');
 
             if (!this.config.resizeRestriction) {
                 this.config.resizeRestriction = this.getContainer()[0];
