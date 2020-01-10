6af5fa3f1ec56ec26bdcf944de6df592c4d176a5
Martin Nicholson <martin@taotesting.com>
2020-01-10 14:31:42 +0100

    Revert config change & remove aliases

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -89,8 +89,6 @@ export default inputs.map(input => {
                     module: Handlebars
                 },
                 helpers: ['build/tpl.js'],
-                isPartial : () => true,
-                partialRoot: srcDir,
                 templateExtension: '.tpl'
             }),
             /**
