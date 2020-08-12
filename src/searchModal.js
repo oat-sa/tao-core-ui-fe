d903832f38d57ae290d3a9e15f9966529646d440
Adrian Lopez <adrian.lopezma@gmail.com>
2020-08-12 13:52:58 +0200

    chore(search-modal-component):implemented component creation and destroy

---
+++ src/searchModal.js
@@ -0,0 +1,56 @@
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2020 (original work) Open Assessment Technologies SA ;
+ */
+import $ from 'jquery';
+import layoutTpl from 'ui/searchModal/tpl/layout';
+import 'ui/searchModal/css/searchModal.css';
+import component from 'ui/component';
+import 'ui/modal';
+
+export default function searchModalFactory(config) {
+    const instance = component().setTemplate(layoutTpl)
+        .on('render', () => renderModal())
+        .on('destroy', () => destroyModal());
+
+    function renderModal() {
+        instance
+        .getElement()
+        .addClass('modal')
+        .on('closed.modal', function() {
+            instance.destroy();
+        })
+        .modal({
+            disableEscape: true,
+            width: $( window ).width(),
+            minHeight: $( window ).height(),
+            top: 1, // can not be set to 0 because on modal.js:230 a 0 is a falsy value so default top of 40 px is applied
+            modalCloseClass: 'modal-close-left'
+        })
+        .focus();
+    }
+
+    function destroyModal() {
+        if (instance.getElement()) { // for some reason destroy is triggered twice, so withouth this condition it fails in the second one
+            instance.getElement()
+                .removeClass('modal')
+                .modal('destroy');
+            $('.modal-bg').remove();
+        }
+    }
+
+    return instance.init({renderTo:'body'});
+}
---
+++ src/searchModal/tpl/layout.tpl
@@ -0,0 +1,3 @@
+<div class="search-modal">
+    <h1>Hello world!</h1>
+</div>
