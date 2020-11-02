4db377c50dd2b1dfc589baea4e4534e9a64c8b55
Hanna Dzmitryieva <bziondik@gmail.com>
2020-11-02 18:11:10 +0300

    remove tpl extension

--- src/dialog/confirmDelete.js
+++ src/dialog/confirmDelete.js
@@ -21,7 +21,7 @@
 import _ from 'lodash';
 import __ from 'i18n';
 import dialog from 'ui/dialog';
-import checkBoxTpl from 'ui/dialog/tpl/checkBox.tpl';
+import checkBoxTpl from 'ui/dialog/tpl/checkBox';
 
 /**
  * Displays a confirm message
