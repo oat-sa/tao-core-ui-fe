cd912250b646ebfeed9a875552e3e1e1644154a2
404passionfruit <404passionFruit@gmail.com>
2021-05-28 10:08:29 +0300

    feat: ADF-39 alignment plugin

---
+++ src/mediaEditor/plugins/mediaAlignment/mediaAlignmentComponent.js
@@ -0,0 +1,114 @@
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2021  (original work) Open Assessment Technologies SA;
+ *
+ * @author Juan Luis Gutierrez Dos Santos <juanluis.gutierrezdossantos@taotesting.com>
+ */
+
+/**
+ * Controls media size
+ */
+ import $ from 'jquery';
+ import _ from 'lodash';
+ import component from 'ui/component';
+ import tpl from 'ui/mediaEditor/plugins/mediaAlignment/tpl/mediaAlignment';
+ import 'ui/mediaEditor/plugins/mediaAlignment/style.css';
+ 
+ /**
+  * Creates mediaAlignment component
+  * @param $container
+  * @param media
+  * @param config
+  * @fires "changed" - on State changed
+  * return {component|*}
+  */
+ export default function mediaAlignmentFactory($container, media) {
+     /**
+      * Template of the dimension controller
+      */
+     let $template;
+ 
+     /**
+      * Size properties of the media control panel
+      * @typedef {Object} mediaSizeProps
+      * @property showResponsiveToggle boolean
+      * @property responsive boolean
+      * @property sizeProps sizeProps
+      * @property syncDimensions boolean
+      * @property denyCustomRatio boolean
+      * @property precision number
+      * @property showReset boolean
+      */
+ 
+     /**
+      * Current component
+      */
+     const mediaAlignmentComponent = component(
+         {
+             /**
+              * Apply configurations to the view
+              */
+             update(conf) {
+                 $template.find('input:checked').prop('checked', false);
+                 switch (conf) {
+                     case 'wrap-right':
+                         conf = 'right';
+                         $template.find('input[name="wrap-right"]').prop('checked', true);
+                         break;
+                     case 'wrap-left':
+                         conf = 'left';
+                         $template.find('input[name="wrap-left"]').prop('checked', true);
+                         break;
+                     default:
+                         conf = 'default';
+                         $template.find('input[name="wrap-inline"]').prop('checked', true);
+                         break;
+                 }
+                 media.align = conf;
+                 this.trigger('change', media);
+             }
+         }
+     );
+ 
+     mediaAlignmentComponent
+         .on('init', function() {
+             this.render($container);
+         })
+         .on('render', function () {
+             $template = $(tpl());
+             $template.appendTo($container);
+             const classListTag = media.$node[0].parentNode.classList;
+ 
+             if (classListTag.contains('rgt')) {
+                 this.update('wrap-right');
+             } else if (classListTag.contains('lft')) {
+                 this.update('wrap-left');
+             } else {
+                 this.update('wrap-inline');
+             }
+ 
+             $template.on('click', event => {
+                 event.target.name && this.update(event.target.name);
+             });
+         })
+         .on('destroy', function() {
+             $template.remove();
+         });
+ 
+     _.defer(() => mediaAlignmentComponent.init());
+ 
+     return mediaAlignmentComponent;
+ }
---
+++ src/mediaEditor/plugins/mediaAlignment/tpl/mediaAlignment.tpl
@@ -0,0 +1,25 @@
+<div class="media-alignment">
+    <fieldset>
+        <legend class="media-sizer-panel-label">{{__ 'Position'}}</legend>
+        <label class="smaller-prompt">
+            <input type="radio" name="wrap-inline"/>
+            <span class="icon-radio"></span>
+            <span class="icon-wrap-inline"></span>
+            {{__ 'Inline'}}
+        </label>
+        <br>
+        <label class="smaller-prompt">
+            <input type="radio" name="wrap-left"/>
+            <span class="icon-radio"></span>
+            <span class="icon-wrap-left"></span>
+            {{__ 'Wrap image left'}}
+        </label>
+        <br>
+        <label class="smaller-prompt">
+            <input type="radio" name="wrap-right"/>
+            <span class="icon-radio"></span>
+            <span class="icon-wrap-right"></span>
+            {{__ 'Wrap image right'}}
+        </label>
+    </fieldset>
+</div>
