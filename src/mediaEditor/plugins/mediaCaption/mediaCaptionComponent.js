191905a48e4a0d8f2010dbb1aed96c19e4b31436
maria.cano <maria.cano@taotesting.com>
2022-05-11 16:41:54 +0200

    feat: add mediaCaption plugin to mediaEditor

---
+++ src/mediaEditor/plugins/mediaCaption/mediaCaptionComponent.js
@@ -0,0 +1,91 @@
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2018  (original work) Open Assessment Technologies SA;
+ */
+
+/**
+ * Controls media size
+ */
+import $ from 'jquery';
+import _ from 'lodash';
+import component from 'ui/component';
+import tpl from 'ui/mediaEditor/plugins/mediaCaption/tpl/mediaCaption';
+import 'nouislider';
+import 'ui/tooltip';
+import 'ui/mediaEditor/plugins/mediaCaption/style.css';
+
+/**
+ * Creates mediaCaption component
+ * @param {jQueryElement} $container
+ * @param {Object} media
+ * @param {Object} config
+ * @fires "changed" - on State changed
+ * @returns {component|*} mediaCaptionComponent
+ */
+export default function mediaCaptionFactory($container, media) {
+    /**
+     * Template of the dimension controller
+     */
+    let $template;
+
+    /**
+     * Current component
+     */
+    const mediaCaptionComponent = component({
+        /**
+         * Apply configurations to the view
+         */
+        update(caption) {
+            $template = $(tpl({
+                figcaption: caption
+            }));
+            this.trigger('change', {
+                figcaption: caption
+            });
+        }
+    });
+
+    mediaCaptionComponent
+        .on('init', function () {
+            media.figcaption =
+                typeof media.figcaption !== 'undefined' ? media.figcaption : '';
+            this.render($container);
+        })
+        .on('render', function () {
+            $template = $(
+                tpl({
+                    figcaption: media.figcaption
+                })
+            );
+            $template.appendTo(this.getContainer());
+
+            mediaCaptionComponent.update(media.figcaption);
+
+            const textarea = $template.find('textarea');
+            $template.on('change keyup input', '#figcaption', (event) => {
+                event.target.value && this.update(event.target.value);
+            }, false);
+        })
+        .on('destroy', function () {
+            $template.remove();
+        });
+
+    _.defer(function () {
+        mediaCaptionComponent.init();
+    });
+
+    return mediaCaptionComponent;
+}
---
+++ src/mediaEditor/plugins/mediaCaption/tpl/mediaCaption.tpl
@@ -0,0 +1,6 @@
+<div class="panel">
+    <label for="figcaption">{{__ "Image caption"}}</label>
+    <span class="icon-help tooltipstered" data-tooltip="~ .tooltip-content:first" data-tooltip-theme="info"></span>
+    <div class="tooltip-content">{{__ 'The text to be displayed below the image.'}}</div>
+    <textarea name="figcaption" id="figcaption" cols="100%" rows="5">{{figcaption}}</textarea>
+</div>
