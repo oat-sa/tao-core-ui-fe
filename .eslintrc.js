5afef4a1b3628b45c1f79de5c3cc26014ddb14c9
Andrey Shaveko <shaveko@gmail.com>
2022-10-25 12:33:11 +0200

    fix: add @babel/eslint-parser to support object spread syntax

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,8 @@
 module.exports = {
     root: true,
+    parser: '@babel/eslint-parser',
+    parserOptions: {
+        requireConfigFile: false
+    },
     extends: '@oat-sa/eslint-config-tao'
 };
