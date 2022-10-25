9ebde8286fe2fab2975f6c32327b88dea950c122
Andrey Shaveko <shaveko@gmail.com>
2022-10-25 16:25:46 +0200

    fix: eslint durationer/test

--- test/dropdown/test.js
+++ test/dropdown/test.js
@@ -18,7 +18,7 @@
 /**
  * @author Martin Nicholson <martin@taotesting.com>
  */
-define(['jquery', 'ui/dropdown'], function($, dropdown) {
+define(['jquery', 'ui/dropdown'], function ($, dropdown) {
     'use strict';
 
     var htmlItem = '<a><span>HTML Item</span></a>';
@@ -39,7 +39,7 @@ define(['jquery', 'ui/dropdown'], function($, dropdown) {
 
     QUnit.module('dropdown');
 
-    QUnit.test('module', function(assert) {
+    QUnit.test('module', function (assert) {
         assert.equal(typeof dropdown, 'function', 'The dropdown module exposes a function');
         assert.equal(typeof dropdown(), 'object', 'The dropdown factory produces an object');
         assert.notStrictEqual(dropdown(), dropdown(), 'The dropdown factory provides a different object on each call');
@@ -66,16 +66,16 @@ define(['jquery', 'ui/dropdown'], function($, dropdown) {
             { title: 'removeItem' },
             { title: 'clearItems' }
         ])
-        .test('instance API ', function(data, assert) {
+        .test('instance API ', function (data, assert) {
             var instance = dropdown();
             assert.equal(
                 typeof instance[data.title],
                 'function',
-                `The dropdown instance exposes a "${  data.title  }" function`
+                `The dropdown instance exposes a "${data.title}" function`
             );
         });
 
-    QUnit.test('creation', function(assert) {
+    QUnit.test('creation', function (assert) {
         var dd = dropdown({
             renderTo: '#fixture-init',
             isOpen: false
@@ -86,33 +86,21 @@ define(['jquery', 'ui/dropdown'], function($, dropdown) {
         assert.equal(dd.getElement().length, 1, 'The dropdown instance gets a single element');
 
         assert.equal(
-            dd
-                .getElement()
-                .find('.dropdown-header')
-                .children().length,
+            dd.getElement().find('.dropdown-header').children().length,
             0,
             'The dropdown header has no children'
         );
         assert.equal(
-            dd
-                .getElement()
-                .find('.dropdown-submenu')
-                .children().length,
+            dd.getElement().find('.dropdown-submenu').children().length,
             0,
             'The dropdown list has no children'
         );
-        assert.ok(
-            !dd
-                .getElement()
-                .find('.dropdown')
-                .hasClass('open'),
-            'The dropdown is not open'
-        );
+        assert.ok(!dd.getElement().find('.dropdown').hasClass('open'), 'The dropdown is not open');
 
         dd.destroy();
     });
 
-    QUnit.test('basic mouse events 1', function(assert) {
+    QUnit.test('basic mouse events 1', function (assert) {
         var dd1 = dropdown({
             renderTo: '#fixture-mouse1',
             activatedBy: 'hover',
@@ -127,28 +115,25 @@ define(['jquery', 'ui/dropdown'], function($, dropdown) {
         dd1.destroy();
     });
 
-    QUnit.test('basic mouse events 2', function(assert) {
+    QUnit.test('basic mouse events 2', function (assert) {
         var dd2 = dropdown({
             renderTo: '#fixture-mouse2',
             activatedBy: 'click',
             isOpen: false
         });
 
-        dd2.getElement()
-            .find('.dropdown-header')
-            .trigger('click');
+        dd2.getElement().find('.dropdown-header').trigger('click');
         assert.equal(dd2.is('open'), true, 'The menu opens on click');
-        dd2.getElement()
-            .find('.dropdown-header')
-            .trigger('click');
+        dd2.getElement().find('.dropdown-header').trigger('click');
         assert.equal(dd2.is('open'), false, 'The menu closes on click');
 
         dd2.destroy();
     });
 
-    QUnit.test('event triggering', function(assert) {
-        var ready1 = assert.async();
-        var dd3 = dropdown(
+    QUnit.test('event triggering', function (assert) {
+        const ready1 = assert.async();
+        const ready = assert.async();
+        const dd3 = dropdown(
             {
                 renderTo: '#fixture-mouse3',
                 activatedBy: 'click',
@@ -159,26 +144,23 @@ define(['jquery', 'ui/dropdown'], function($, dropdown) {
                 items: items
             }
         );
-        dd3.on('item-click', function() {
+        dd3.on('item-click', function () {
             assert.ok(true, 'The component fires a generic event when an item is clicked');
             ready();
-        }).on('item-click-second', function() {
+        }).on('item-click-second', function () {
             assert.ok(true, 'The component fires a specific event when an item with an id is clicked');
             ready1();
         });
 
         assert.expect(2);
-        var ready = assert.async();
 
-        dd3.getElement()
-            .find('li:nth-child(2)')
-            .trigger('click');
+        dd3.getElement().find('li:nth-child(2)').trigger('click');
 
         dd3.destroy();
     });
 
-    QUnit.test('adding html items', function(assert) {
-        var dd4 = dropdown({
+    QUnit.test('adding html items', function (assert) {
+        const dd4 = dropdown({
             renderTo: '#fixture-html-items',
             isOpen: false
         });
@@ -186,19 +168,9 @@ define(['jquery', 'ui/dropdown'], function($, dropdown) {
         dd4.setHeader(htmlItem);
         dd4.addItem({ content: htmlItem });
 
+        assert.equal(dd4.getElement().find('.dropdown-header span').text(), 'HTML Item', 'The dropdown header was set');
         assert.equal(
-            dd4
-                .getElement()
-                .find('.dropdown-header span')
-                .text(),
-            'HTML Item',
-            'The dropdown header was set'
-        );
-        assert.equal(
-            dd4
-                .getElement()
-                .find('.dropdown-submenu li:last-child span')
-                .text(),
+            dd4.getElement().find('.dropdown-submenu li:last-child span').text(),
             'HTML Item',
             'The dropdown list item was set'
         );
@@ -213,7 +185,7 @@ define(['jquery', 'ui/dropdown'], function($, dropdown) {
         dd4.destroy();
     });
 
-    QUnit.test('removing items', function(assert) {
+    QUnit.test('removing items', function (assert) {
         var dd5 = dropdown({
             renderTo: '#fixture-removal',
             isOpen: false
@@ -240,7 +212,7 @@ define(['jquery', 'ui/dropdown'], function($, dropdown) {
     // Leave some instances alive for user testing:
     QUnit.module('Visual');
 
-    QUnit.test('playground', function(assert) {
+    QUnit.test('playground', function (assert) {
         dropdown(
             {
                 renderTo: '#visual-test-hover',
