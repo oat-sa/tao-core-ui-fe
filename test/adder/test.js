15b01140b30439427a9f7b3f9e6fcbd1b0348012
Andrey Shaveko <shaveko@gmail.com>
2022-10-25 14:10:06 +0200

    fix: eslint adder/test

--- test/adder/test.js
+++ test/adder/test.js
@@ -1,28 +1,28 @@
-define(['jquery', 'ui/adder'], function($, adder) {
+define(['jquery', 'ui/adder'], function ($, adder) {
     QUnit.module('Adder Stand Alone Test');
 
-    QUnit.test('plugin', function(assert) {
+    QUnit.test('plugin', function (assert) {
         assert.expect(1);
         assert.ok(typeof $.fn.adder === 'function', 'The Adder plugin is registered');
     });
 
-    QUnit.test('Initialization', function(assert) {
+    QUnit.test('Initialization', function (assert) {
         var ready = assert.async();
         assert.expect(6);
 
-        var $container = $('#container-1');
+        const $container = $('#container-1');
         assert.ok($container.length === 1, 'Test the fixture is available');
 
-        var $elt = $('.adder', $container);
+        const $elt = $('.adder', $container);
         assert.ok($elt.length === 1, 'Adder link is available');
 
-        var $target = $('.content', $container);
+        const $target = $('.content', $container);
         assert.ok($target.length === 1, 'Target is available');
 
-        var $tmpl = $('.tmpl', $container);
+        const $tmpl = $('.tmpl', $container);
         assert.ok($tmpl.length === 1, 'Template is available');
 
-        $elt.on('create.adder', function() {
+        $elt.on('create.adder', function () {
             var data = $elt.data('ui.adder');
             assert.ok(typeof data === 'object', 'The element is runing the plugin');
 
@@ -37,26 +37,26 @@ define(['jquery', 'ui/adder'], function($, adder) {
         });
     });
 
-    QUnit.test('Template adding', function(assert) {
+    QUnit.test('Template adding', function (assert) {
         var ready = assert.async();
         assert.expect(7);
 
-        var $container = $('#container-1');
+        const $container = $('#container-1');
         assert.ok($container.length === 1, 'Test the fixture is available');
 
-        var $elt = $('.adder', $container);
+        const $elt = $('.adder', $container);
         assert.ok($elt.length === 1, 'Adder link is available');
 
-        var $target = $('.content', $container);
+        const $target = $('.content', $container);
         assert.ok($target.length === 1, 'Target is available');
 
-        var $tmpl = $('.tmpl', $container);
+        const $tmpl = $('.tmpl', $container);
         assert.ok($tmpl.length === 1, 'Template is available');
 
-        $elt.on('create.adder', function() {
+        $elt.on('create.adder', function () {
             $elt.trigger('click');
         });
-        $elt.on('add.adder', function() {
+        $elt.on('add.adder', function () {
             var $p = $target.find('p');
             assert.ok($p.length === 1, 'Content is added');
 
@@ -68,7 +68,7 @@ define(['jquery', 'ui/adder'], function($, adder) {
         $elt.adder({
             target: $target,
             content: $tmpl,
-            templateData: function(cb) {
+            templateData: function (cb) {
                 cb({
                     id: 'foo',
                     text: 'bar'
@@ -77,30 +77,30 @@ define(['jquery', 'ui/adder'], function($, adder) {
         });
     });
 
-    QUnit.test('HTML adding', function(assert) {
+    QUnit.test('HTML adding', function (assert) {
         var ready = assert.async();
         assert.expect(5);
 
-        var $container = $('#container-2');
+        const $container = $('#container-2');
         assert.ok($container.length === 1, 'Test the fixture is available');
 
-        var $elt = $('.adder', $container);
+        const $elt = $('.adder', $container);
         assert.ok($elt.length === 1, 'Adder link is available');
 
-        var $target = $('.list2', $container);
+        const $target = $('.list2', $container);
         assert.ok($target.length === 1, 'Target is available');
 
-        var $content = $('.list1', $container);
+        const $content = $('.list1', $container);
         assert.ok($content.length === 1, 'DOM content is available');
 
-        $elt.on('create.adder', function() {
+        $elt.on('create.adder', function () {
             $elt.trigger('click');
-            setTimeout(function() {
+            setTimeout(function () {
                 $elt.trigger('click');
             }, 500);
         });
-        var counter = 0;
-        $elt.on('add.adder', function() {
+        let counter = 0;
+        $elt.on('add.adder', function () {
             if (counter >= 1) {
                 assert.ok($target.find('li').length === 2, 'HTML content added 2 times');
                 ready();
@@ -115,17 +115,17 @@ define(['jquery', 'ui/adder'], function($, adder) {
 
     QUnit.module('Adder Data Attr Test');
 
-    QUnit.test('initialization', function(assert) {
-        var ready = assert.async();
+    QUnit.test('initialization', function (assert) {
+        const ready = assert.async();
         assert.expect(3);
 
-        var $container = $('#container-3');
+        const $container = $('#container-3');
         assert.ok($container.length === 1, 'Test the fixture is available');
 
-        var $elt = $('.adder', $container);
+        const $elt = $('.adder', $container);
         assert.ok($elt.length === 1, 'Test input is available');
 
-        $elt.on('add.adder', function() {
+        $elt.on('add.adder', function () {
             assert.ok($('#c3-list2').find('li').length === 1, 'Content added');
             ready();
         });
