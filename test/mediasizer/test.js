6243d9cff2e2fe015752da95b664e59a3ddf7df8
Andrey Shaveko <shaveko@gmail.com>
2022-10-27 15:18:17 +0200

    fix: eslint mediasizer/test

--- test/mediasizer/test.js
+++ test/mediasizer/test.js
@@ -1,29 +1,29 @@
-define(['jquery', 'ui/mediasizer'], function($) {
+define(['jquery', 'ui/mediasizer'], function ($) {
     var mode = 'nat'; // Nat (=natural size) | px (=pixels) | pc (=%)
 
-    $('#_' + mode).show();
+    $(`#_${mode}`).show();
 
     QUnit.module('MediaSizer Stand Alone Test');
 
-    QUnit.test('plugin', function(assert) {
+    QUnit.test('plugin', function (assert) {
         assert.expect(1);
         assert.ok(typeof $.fn.mediasizer === 'function', 'The MediaSizer plugin is registered');
     });
 
-    QUnit.test('Initialization', function(assert) {
-        var ready = assert.async();
+    QUnit.test('Initialization', function (assert) {
+        const ready = assert.async();
         assert.expect(4);
 
-        var $container = $('#qunit-fixture-tmp');
+        const $container = $('#qunit-fixture-tmp');
         assert.ok($container.length === 1, 'Fixture is available');
 
-        var $elt = $('#media-sizer-container', $container);
+        const $elt = $('#media-sizer-container', $container);
         assert.ok($elt.length === 1, 'MediaSizer link is available');
 
-        var $target = $('#_' + mode + ' img', $container);
+        const $target = $(`#_${mode} img`, $container);
         assert.ok($target.length === 1, 'Target is available');
 
-        $elt.on('create.mediasizer', function() {
+        $elt.on('create.mediasizer', function () {
             var data = $elt.data('ui.mediasizer');
             assert.ok(typeof data === 'object', 'The element is running the plugin');
 
@@ -33,7 +33,8 @@ define(['jquery', 'ui/mediasizer'], function($) {
             target: $target
         });
 
-        $elt.on('sizechange.mediasizer', function(e, paras) {
+        $elt.on('sizechange.mediasizer', function (e, paras) {
+            //eslint-disable-next-line no-console
             console.log(paras);
         });
     });
