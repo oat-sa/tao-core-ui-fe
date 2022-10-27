0cf0969137b662adcf9738fea2c33fb3122b608f
Andrey Shaveko <shaveko@gmail.com>
2022-10-27 15:21:23 +0200

    fix: eslint modal/test

--- test/modal/test.js
+++ test/modal/test.js
@@ -18,39 +18,39 @@
 /**
  * @author Jean-Sébastien Conan <jean-sebastien.conan@vesperiagroup.com>
  */
-define(['jquery', 'lodash', 'ui/modal', 'tpl!test/ui/modal/modal'], function($, _, modalListener, modalTpl) {
+define(['jquery', 'lodash', 'ui/modal', 'tpl!test/ui/modal/modal'], function ($, _, modalListener, modalTpl) {
     'use strict';
 
     QUnit.module('modal');
 
-    QUnit.test('module', function(assert) {
+    QUnit.test('module', function (assert) {
         assert.equal(typeof modalListener, 'function', 'The modal module exposes a function');
         assert.equal(typeof $().modal, 'function', "The modal module inject a jQuery plugin named 'modal'");
     });
 
-    QUnit.test('install', function(assert) {
-        var ready2 = assert.async();
-        var ready1 = assert.async();
-        var $container = $('#modal-1');
-        var $modal = $(modalTpl());
+    QUnit.test('install', function (assert) {
+        const ready2 = assert.async();
+        const ready1 = assert.async();
+        const ready = assert.async();
+        const $container = $('#modal-1');
+        const $modal = $(modalTpl());
         $container.append($modal);
 
-        $modal.on('opened.modal', function() {
+        $modal.on('opened.modal', function () {
             assert.ok(true, 'The modal is now visible');
             ready();
 
             $modal.modal('close');
         });
-        $modal.on('closed.modal', function() {
+        $modal.on('closed.modal', function () {
             assert.ok(true, 'The modal is now hidden');
             ready1();
         });
-        $modal.on('create.modal', function() {
+        $modal.on('create.modal', function () {
             assert.ok(true, 'The modal is created');
             ready2();
         });
 
-        var ready = assert.async();
         $modal.modal();
 
         assert.equal($container.children().length, 2, 'The modal append 2 elements');
@@ -64,34 +64,35 @@ define(['jquery', 'lodash', 'ui/modal', 'tpl!test/ui/modal/modal'], function($, 
         assert.equal($modal.find('.modal-close').length, 1, 'The modal appends a close button');
     });
 
-    QUnit.test('destroy', function(assert) {
-        var ready2 = assert.async();
-        var ready1 = assert.async();
-        var $container = $('#modal-2');
-        var $modal = $(modalTpl());
+    QUnit.test('destroy', function (assert) {
+        const ready2 = assert.async();
+        const ready1 = assert.async();
+        const ready = assert.async();
+        const $container = $('#modal-2');
+        const $modal = $(modalTpl());
+
         $container.append($modal);
 
-        $modal.on('opened.modal', function() {
+        $modal.on('opened.modal', function () {
             assert.ok(true, 'The modal is now visible');
             ready();
 
             $modal.modal('destroy');
         });
-        $modal.on('closed.modal', function() {
+        $modal.on('closed.modal', function () {
             assert.ok(false, 'The modal is must not be hidden when destroyed');
         });
-        $modal.on('create.modal', function() {
+        $modal.on('create.modal', function () {
             assert.ok(true, 'The modal is created');
             ready1();
         });
-        $modal.on('destroyed.modal', function() {
+        $modal.on('destroyed.modal', function () {
             assert.equal($container.find('.modal-bg').length, 0, 'The modal has removed the overlay');
             assert.ok(true, 'The modal is now destroyed');
 
             ready2();
         });
 
-        var ready = assert.async();
         $modal.modal();
 
         assert.equal($container.children().length, 2, 'The modal append 2 elements');
@@ -105,7 +106,7 @@ define(['jquery', 'lodash', 'ui/modal', 'tpl!test/ui/modal/modal'], function($, 
         assert.equal($modal.find('.modal-close').length, 1, 'The modal appends a close button');
     });
 
-    QUnit.test('events', function(assert) {
+    QUnit.test('events', function (assert) {
         var ready3 = assert.async();
         var ready2 = assert.async();
         var ready1 = assert.async();
@@ -116,7 +117,7 @@ define(['jquery', 'lodash', 'ui/modal', 'tpl!test/ui/modal/modal'], function($, 
         var closed = false;
         $container.append($modal);
 
-        $modal.on('opened.modal', function() {
+        $modal.on('opened.modal', function () {
             assert.ok(true, 'The modal is now visible');
             ready();
 
@@ -126,7 +127,7 @@ define(['jquery', 'lodash', 'ui/modal', 'tpl!test/ui/modal/modal'], function($, 
                 $modal.modal('close');
             }
         });
-        $modal.on('closed.modal', function(e, reason) {
+        $modal.on('closed.modal', function (e, reason) {
             assert.ok(true, 'The modal is now hidden');
             assert.equal(typeof e, 'object', 'A event object is provided');
             assert.equal(reason, 'api', 'The exit reason has been provided');
@@ -135,11 +136,11 @@ define(['jquery', 'lodash', 'ui/modal', 'tpl!test/ui/modal/modal'], function($, 
             closed = true;
             $modal.modal('open');
         });
-        $modal.on('create.modal', function() {
+        $modal.on('create.modal', function () {
             assert.ok(true, 'The modal is created');
             ready2();
         });
-        $modal.on('destroyed.modal', function() {
+        $modal.on('destroyed.modal', function () {
             assert.equal($container.find('.modal-bg').length, 0, 'The modal has removed the overlay');
             assert.ok(true, 'The modal is now destroyed');
 
@@ -160,16 +161,17 @@ define(['jquery', 'lodash', 'ui/modal', 'tpl!test/ui/modal/modal'], function($, 
     });
 
     // Modal position does not depend on the main page scroll (BODY OR HTML)
-    QUnit.test('mainScroll', function(assert) {
-        var ready = assert.async();
+    QUnit.test('mainScroll', function (assert) {
+        const ready = assert.async();
+        const $container = $('#modal-1');
+        const $modal = $(modalTpl());
+
         assert.expect(2);
 
-        var $container = $('#modal-1');
-        var $modal = $(modalTpl());
         $modal.css({ position: 'absolute' });
         $container.append($modal);
 
-        $modal.on('opened.modal', function() {
+        $modal.on('opened.modal', function () {
             assert.ok(true, 'The modal is visible');
             assert.equal($modal.css('top'), '40px', 'Default scroll value used for the position');
             ready();
@@ -179,20 +181,22 @@ define(['jquery', 'lodash', 'ui/modal', 'tpl!test/ui/modal/modal'], function($, 
     });
 
     // Modal position should be changed when the modal in the container
-    QUnit.test('innerScroll', function(assert) {
-        var ready = assert.async();
+    QUnit.test('innerScroll', function (assert) {
+        const ready = assert.async();
+        const $container = $('#modal-1');
+        const $modal = $(modalTpl());
+        const $scrolledContainer = $('<div style="height: 200px; overflow: auto;"></div>');
+        const $content = $('<div style="height: 1000px;">Body</div>');
+
         assert.expect(2);
-        var $container = $('#modal-1');
-        var $modal = $(modalTpl());
-        var $scrolledContainer = $('<div style="height: 200px; overflow: auto;"></div>');
-        var $content = $('<div style="height: 1000px;">Body</div>');
+
         $modal.css({ position: 'absolute' });
         $scrolledContainer.append($content);
         $scrolledContainer.append($modal);
         $container.append($scrolledContainer);
         $scrolledContainer.scrollTop(500);
 
-        $modal.on('opened.modal', function() {
+        $modal.on('opened.modal', function () {
             assert.ok(true, 'The modal is visible');
             assert.equal($modal.css('top'), '540px', 'Modal window opened in the scrolled position');
             ready();
