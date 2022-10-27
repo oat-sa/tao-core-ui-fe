52948d942349ec041baaccdb7164d53e6e0571e0
Andrey Shaveko <shaveko@gmail.com>
2022-10-27 15:26:46 +0200

    fix: eslint progressbar/test

--- test/progressbar/test.js
+++ test/progressbar/test.js
@@ -1,24 +1,24 @@
-define(['jquery', 'ui/progressbar'], function($) {
+define(['jquery', 'ui/progressbar'], function ($) {
     'use strict';
 
     var containerId = 'mypg';
 
     QUnit.module('ProgressBar');
 
-    QUnit.test('plugin', function(assert) {
+    QUnit.test('plugin', function (assert) {
         assert.expect(1);
         assert.ok(typeof $.fn.progressbar === 'function', 'The progressbar plugin is registered');
     });
 
-    QUnit.test('Initialization', function(assert) {
-        var ready = assert.async();
-        assert.expect(3);
+    QUnit.test('Initialization', function (assert) {
+        const ready = assert.async();
+        const $elt = $(`#${containerId}`);
 
-        var $elt = $('#' + containerId);
+        assert.expect(3);
 
         assert.ok($elt.length === 1, 'Test the fixture is available');
 
-        $elt.on('create.progressbar', function() {
+        $elt.on('create.progressbar', function () {
             assert.ok($elt.hasClass('progressbar'), 'the element has the right class');
             assert.equal($elt.find('span').length, 1, 'the sub element has been inserted');
 
@@ -27,15 +27,15 @@ define(['jquery', 'ui/progressbar'], function($) {
         $elt.progressbar();
     });
 
-    QUnit.test('Success style', function(assert) {
-        var ready = assert.async();
-        assert.expect(3);
+    QUnit.test('Success style', function (assert) {
+        const ready = assert.async();
+        const $elt = $(`#${containerId}`);
 
-        var $elt = $('#' + containerId);
+        assert.expect(3);
 
         assert.ok($elt.length === 1, 'Test the fixture is available');
 
-        $elt.on('create.progressbar', function() {
+        $elt.on('create.progressbar', function () {
             assert.equal($elt.find('span').length, 1, 'the sub element has been inserted');
             assert.ok($elt.hasClass('success'), 'the sub element has the success CSS class');
 
@@ -46,18 +46,18 @@ define(['jquery', 'ui/progressbar'], function($) {
         });
     });
 
-    QUnit.test('Destroy', function(assert) {
-        var ready = assert.async();
-        assert.expect(5);
+    QUnit.test('Destroy', function (assert) {
+        const ready = assert.async();
+        const $elt = $(`#${containerId}`);
 
-        var $elt = $('#' + containerId);
+        assert.expect(5);
 
         assert.ok($elt.length === 1, 'Test the fixture is available');
 
-        $elt.on('create.progressbar', function() {
+        $elt.on('create.progressbar', function () {
             assert.ok($elt.hasClass('progressbar'), 'the element has the right class');
             assert.equal($elt.find('span').length, 1, 'the sub element has been inserted');
-        }).on('destroy.progressbar', function() {
+        }).on('destroy.progressbar', function () {
             assert.ok(!$elt.hasClass('progressbar'), 'the element class has been removed');
             assert.equal($elt.find('span').length, 0, 'the sub element has been removed');
 
@@ -67,15 +67,15 @@ define(['jquery', 'ui/progressbar'], function($) {
         $elt.progressbar().progressbar('destroy');
     });
 
-    QUnit.test('update', function(assert) {
-        var ready = assert.async();
+    QUnit.test('update', function (assert) {
+        const ready = assert.async();
+        const $elt = $(`#${containerId}`);
+        
         assert.expect(3);
-
-        var $elt = $('#' + containerId);
 
         assert.ok($elt.length === 1, 'Test the fixture is available');
 
-        $elt.on('update.progressbar', function(e, value) {
+        $elt.on('update.progressbar', function (e, value) {
             assert.equal(value, 55, 'the given value matches');
             assert.equal($elt.find('span')[0].style.width, '55%', 'the sub element width matches the value');
 
@@ -85,15 +85,15 @@ define(['jquery', 'ui/progressbar'], function($) {
         $elt.progressbar().progressbar('update', 55);
     });
 
-    QUnit.test('set value', function(assert) {
-        var ready = assert.async();
+    QUnit.test('set value', function (assert) {
+        const ready = assert.async();
+        const $elt = $(`#${containerId}`);
+        
         assert.expect(3);
-
-        var $elt = $('#' + containerId);
 
         assert.ok($elt.length === 1, 'Test the fixture is available');
 
-        $elt.on('update.progressbar', function(e, value) {
+        $elt.on('update.progressbar', function (e, value) {
             assert.equal(value, 42, 'the given value matches');
             assert.equal($elt.find('span')[0].style.width, '42%', 'the sub element width matches the value');
 
@@ -103,15 +103,15 @@ define(['jquery', 'ui/progressbar'], function($) {
         $elt.progressbar().progressbar('value', 42);
     });
 
-    QUnit.test('get value', function(assert) {
-        var ready = assert.async();
+    QUnit.test('get value', function (assert) {
+        const ready = assert.async();
+        const $elt = $(`#${containerId}`);
+        
         assert.expect(2);
-
-        var $elt = $('#' + containerId);
 
         assert.ok($elt.length === 1, 'Test the fixture is available');
 
-        $elt.on('update.progressbar', function() {
+        $elt.on('update.progressbar', function () {
             assert.equal($elt.progressbar('value'), 66, 'Get the current progress value');
 
             ready();
@@ -120,15 +120,16 @@ define(['jquery', 'ui/progressbar'], function($) {
         $elt.progressbar().progressbar('value', 66);
     });
 
-    QUnit.test('show progress', function(assert) {
-        var ready = assert.async();
-        assert.expect(2);
+    QUnit.test('show progress', function (assert) {
+        const ready = assert.async();
+        const $elt = $(`#${containerId}`);
 
-        var $elt = $('#' + containerId);
+        assert.expect(2);
 
         assert.ok($elt.length === 1, 'Test the fixture is available');
 
-        $elt.on('update.progressbar', function(e, value) {
+/**
+        $elt.on('update.progressbar', function () {
             assert.equal($elt.find('span').text(), '38%', 'the sub element contains the progress text');
 
             ready();
